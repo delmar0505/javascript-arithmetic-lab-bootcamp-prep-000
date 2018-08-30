dca3b89fdfa24e2c97815114022fba8c6db51079
oscar leon <first.liberal.thinker@gmail.com>
2018-08-30 01:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+function increments(n){
+  return n++;
+}
+function decrements(a,b){
+  return n--;
+}
+function (a,b){
+  
+}
+function (a,b){
+  
+}
